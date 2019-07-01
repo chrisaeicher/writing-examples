470290a03be46d01a4bd50979061ae6dd7004e51
Chris <chrisaeicher@gmail.com>
2019-07-01 00:43:05 -0600

    hide example box until click

--- js/functions.js
+++ js/functions.js
@@ -4,27 +4,29 @@ var article_button = document.getElementById('article_button');
 var keywords_button = document.getElementById('keywords_button');
 
 // samples
+var sample_div = document.getElementsByClassName('writing-samples')[0];
 var desc = document.getElementById('desc');
 var article = document.getElementById('article');
 var keywords = document.getElementById('keywords');
 
 // onclick handlers
 desc_button.onclick = function() {
+  sample_div.style = "display:block;";
   desc.style = "display: block;";
   article.style = "display: none;";
   keywords.style = "display: none;"
 }
 
 article_button.onclick = function() {
+  sample_div.style = "display:block;";
   article.style = "display: block";
   desc.style = "display: none;";
   keywords.style = "display:none;"
 }
 
 keywords_button.onclick = function() {
+  sample_div.style = "display:block;";
   keywords.style = "display: block;";
   desc.style = "display: none;";
   article.style = "display: none;";
 }
-
-desc_button.click();
